4e072a2919165817c9dbd52055f263f462c6dd07
prateek1556 <ppanjwani0751@gmail.com>
2018-03-14 15:06:11 +0530

    Add POST /user/login route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -115,6 +116,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me',authenticate,(req,res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req,res) => {
+    var body = _.pick(req.body, ['email','password']);
+    
+    User.findByCredentials(body.email,body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
